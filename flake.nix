9346d7ad60c4bb155e3868385d7c8f974280ff25
luisnquin <luisnquin@users.noreply.github.com>
2024-11-10 01:23:44 +0000

    Update Zen Browser to v1.0.1-a.18

--- flake.nix
+++ flake.nix
@@ -10,13 +10,13 @@
     nixpkgs,
   }: let
     system = "x86_64-linux";
-    version = "1.0.1-a.17";
+    version = "1.0.1-a.18";
     downloadUrl = {
       specific.url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-specific.tar.bz2";
-      specific.sha256 = "0ahpig3kafphg0pnkl1r60b9phfp5s2rilpxb20q2fwz41d1miwi";
+      specific.sha256 = "1n6z8wx8w7dhir8vaqbc19ibhsc6g6rma4kvx2nw04l8b4hxg9bg";
 
       generic.url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-generic.tar.bz2";
-      generic.sha256 = "1n1cq0j8hifvwanqs3wsy5q69w04h397q09adxmdbydm6m8xn5k0";
+      generic.sha256 = "05r774avc0i88bgs2qsilagfdnyh5p435sqr3cj71g1hwljw0r4n";
     };
 
     pkgs = import nixpkgs {

5c342274b37fc1ed8a9d2770fde3f53b489b2067
Luis Quiñones <lpaandres2020@gmail.com>
2024-11-29 21:30:20 -0500

    ci(github/new-version): removed control characters for compatibility with jq

--- .github/new-version.sh
+++ .github/new-version.sh
@@ -4,7 +4,7 @@ echo "Fetching upstream version from GitHub API..." >&2
 upstream_data=$(curl -s https://api.github.com/repos/zen-browser/desktop/releases/latest)
 echo "Upstream data: $upstream_data" >&2
 
-upstream=$(echo "$upstream_data" | jq -r '.tag_name')
+upstream=$(echo "$upstream_data" | tr -d '\000-\037\177' | jq -r '.tag_name')
 echo "Upstream version is: $upstream" >&2
 
 local=$(grep -oP 'version = "\K[^"]+' flake.nix)

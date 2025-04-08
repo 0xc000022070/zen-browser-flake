fc288d87ebb8c6ef451c8397258bac1f70486f8d
Gavin John <gavinnjohn@gmail.com>
2025-04-08 05:58:18 -0700

    refactor: move package definitions to default.nix (#40)
    This allows this to be NUR-compatible.

--- flake.nix
+++ flake.nix
@@ -12,27 +12,6 @@
     nixpkgs,
     home-manager,
   }: let
-    mkZen = name: system: entry: let
-      pkgs = nixpkgs.legacyPackages.${system};
-      variant = (builtins.fromJSON (builtins.readFile ./sources.json)).${entry}.${system};
-
-      desktopFile =
-        if name == "beta"
-        then "zen.desktop"
-        else "zen_${name}.desktop";
-    in
-      pkgs.callPackage ./package.nix {
-        inherit name desktopFile variant;
-      };
-
-    mkZenWrapped = name: system: entry: let
-      pkgs = nixpkgs.legacyPackages.${system};
-    in
-      pkgs.wrapFirefox entry {
-        icon = "zen-${name}";
-        wmClass = "zen-${name}";
-        hasMozSystemDirPatch = false;
-      };
 
     supportedSystems = [
       "x86_64-linux"
@@ -41,16 +20,8 @@
 
     forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
   in {
-    packages = forAllSystems (system: rec {
-      beta-unwrapped = mkZen "beta" system "beta";
-      twilight-unwrapped = mkZen "twilight" system "twilight";
-      twilight-official-unwrapped = mkZen "twilight" system "twilight-official";
-
-      beta = mkZenWrapped "beta" system beta-unwrapped;
-      twilight = mkZenWrapped "twilight" system twilight-unwrapped;
-      twilight-official = mkZenWrapped "twilight" system twilight-official-unwrapped;
-
-      default = beta;
+    packages = forAllSystems (system: import ./default.nix {
+      pkgs = nixpkgs.legacyPackages.${system};
     });
 
     formatter = forAllSystems (

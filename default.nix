e2336d4066aa6c79ed0aea80b0e93c2375a86982
Gavin John <gavinnjohn@gmail.com>
2025-04-08 01:11:17 -0700

    Move package definitions to default.nix
    This allows this to be NUR-compatible

---
+++ default.nix
@@ -0,0 +1,35 @@
+{
+  pkgs ? import <nixpkgs> { },
+}:
+
+let
+  mkZen =
+    pkgs: name: system: entry:
+    let
+      variant = (builtins.fromJSON (builtins.readFile ./sources.json)).${entry}.${system};
+
+      desktopFile = if name == "beta" then "zen.desktop" else "zen_${name}.desktop";
+    in
+    pkgs.callPackage ./package.nix {
+      inherit name desktopFile variant;
+    };
+
+  mkZenWrapped =
+    pkgs: name: system: entry:
+    pkgs.wrapFirefox entry {
+      icon = "zen-${name}";
+      wmClass = "zen-${name}";
+      hasMozSystemDirPatch = false;
+    };
+in
+rec {
+  beta-unwrapped = mkZen pkgs "beta" system "beta";
+  twilight-unwrapped = mkZen pkgs "twilight" system "twilight";
+  twilight-official-unwrapped = mkZen pkgs "twilight" system "twilight-official";
+
+  beta = mkZenWrapped pkgs "beta" system beta-unwrapped;
+  twilight = mkZenWrapped pkgs "twilight" system twilight-unwrapped;
+  twilight-official = mkZenWrapped pkgs "twilight" system twilight-official-unwrapped;
+
+  default = beta;
+}

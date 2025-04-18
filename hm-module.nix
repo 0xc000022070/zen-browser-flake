834b349db3ccc426782638e8b8deebb5ad1b6fa5
Dmitry Chermnykh <76gh6rvso@relay.firefox.com>
2025-04-19 01:26:54 +0500

    format code with alejandra

--- hm-module.nix
+++ hm-module.nix
@@ -41,7 +41,7 @@ in {
         # Seems like zen uses relative (to the original binary) path to the policies.json file
         # and ignores the overrides by pkgs.wrapFirefox
         policies = config.programs.zen-browser.policies;
-      }) { };
+      }) {};
       # This does not work, the package can't build using these policies
       policies = lib.mkDefault {
         DisableAppUpdate = true;

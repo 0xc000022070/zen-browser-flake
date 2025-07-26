4d72db5b12171422e2927328cab5e53d4e5734a2
Florian <13469873+VialFlorian@users.noreply.github.com>
2025-07-26 06:04:26 +0200

    feat: add aarch64-darwin (macOS) support (#80)

--- package.nix
+++ package.nix
@@ -18,6 +18,7 @@
   pciutils,
   pipewire,
   adwaita-icon-theme,
+  undmg,
   writeText,
   patchelfUnstable, # have to use patchelfUnstable to support --no-clobber-old-sections
   applicationName ?
@@ -39,6 +40,7 @@
   mozillaPlatforms = {
     x86_64-linux = "linux-x86_64";
     aarch64-linux = "linux-aarch64";
+    aarch64-darwin = "darwin-aarch64";
   };
 
   firefoxPolicies =
@@ -48,36 +50,83 @@
   policiesJson = writeText "firefox-policies.json" (builtins.toJSON {policies = firefoxPolicies;});
 
   pname = "zen-${name}-bin-unwrapped";
+
+  installDarwin = ''
+    mkdir -p "$out/Applications" "$out/bin"
+    cp -r *.app "$out/Applications/${applicationName}.app"
+    ln -s zen "$out/Applications/${applicationName}.app/Contents/MacOS/${binaryName}"
+
+    cat > "$out/bin/${binaryName}" << EOF
+#!/bin/bash
+exec /usr/bin/open -na "$out/Applications/${applicationName}.app" --args "\$@"
+EOF
+
+    chmod +x "$out/bin/${binaryName}"
+    ln -s "$out/bin/${binaryName}" "$out/bin/zen"
+  '';
+
+  installLinux = ''
+    # Linux tarball installation
+    mkdir -p "$prefix/lib/${libName}"
+    cp -r "$src"/* "$prefix/lib/${libName}"
+
+    mkdir -p "$out/bin"
+    ln -s "$prefix/lib/${libName}/zen" "$out/bin/${binaryName}"
+    ln -s "$out/bin/${binaryName}" "$out/bin/zen"
+
+    install -D $desktopSrc/${desktopFile} $out/share/applications/${desktopFile}
+
+    mkdir -p "$out/lib/${libName}/distribution"
+    ln -s ${policiesJson} "$out/lib/${libName}/distribution/policies.json"
+
+    install -D $src/browser/chrome/icons/default/default16.png $out/share/icons/hicolor/16x16/apps/zen-${name}.png
+    install -D $src/browser/chrome/icons/default/default32.png $out/share/icons/hicolor/32x32/apps/zen-${name}.png
+    install -D $src/browser/chrome/icons/default/default48.png $out/share/icons/hicolor/48x48/apps/zen-${name}.png
+    install -D $src/browser/chrome/icons/default/default64.png $out/share/icons/hicolor/64x64/apps/zen-${name}.png
+    install -D $src/browser/chrome/icons/default/default128.png $out/share/icons/hicolor/128x128/apps/zen-${name}.png
+  '';
+
 in
   stdenv.mkDerivation {
     inherit pname;
     inherit (variant) version;
 
-    src = builtins.fetchTarball {inherit (variant) url sha256;};
+    src = if stdenv.hostPlatform.isDarwin
+      then builtins.fetchurl { inherit (variant) url sha256; }
+      else builtins.fetchTarball { inherit (variant) url sha256; };
+
+    sourceRoot = lib.optionalString stdenv.hostPlatform.isDarwin ".";
+
     desktopSrc = ./assets/desktop;
 
-    nativeBuildInputs = [
+    nativeBuildInputs = lib.optionals stdenv.hostPlatform.isLinux [
       wrapGAppsHook3
       autoPatchelfHook
       patchelfUnstable
+    ] ++ lib.optionals stdenv.hostPlatform.isDarwin [
+      undmg
     ];
-    buildInputs = [
+
+    buildInputs = lib.optionals stdenv.hostPlatform.isLinux [
       gtk3
       adwaita-icon-theme
       alsa-lib
       dbus-glib
       libXtst
     ];
-    runtimeDependencies = [
+
+    runtimeDependencies = lib.optionals stdenv.hostPlatform.isLinux [
       curl
       libva.out
       pciutils
       libGL
     ];
-    appendRunpaths = [
+
+    appendRunpaths = lib.optionals stdenv.hostPlatform.isLinux [
       "${libGL}/lib"
       "${pipewire}/lib"
     ];
+
     # Firefox uses "relrhack" to manually process relocations from a fixed offset
     patchelfFlags = ["--no-clobber-old-sections"];
 
@@ -87,26 +136,9 @@ in
       )
     '';
 
-    installPhase = ''
-      mkdir -p "$prefix/lib/${libName}"
-      cp -r "$src"/* "$prefix/lib/${libName}"
-
-      mkdir -p "$out/bin"
-      ln -s "$prefix/lib/${libName}/zen" "$out/bin/${binaryName}"
-      # ! twilight and beta could collide if both are installed
-      ln -s "$out/bin/${binaryName}" "$out/bin/zen"
-
-      install -D $desktopSrc/${desktopFile} $out/share/applications/${desktopFile}
-
-      mkdir -p "$out/lib/${libName}/distribution"
-      ln -s ${policiesJson} "$out/lib/${libName}/distribution/policies.json"
-
-      install -D $src/browser/chrome/icons/default/default16.png $out/share/icons/hicolor/16x16/apps/zen-${name}.png
-      install -D $src/browser/chrome/icons/default/default32.png $out/share/icons/hicolor/32x32/apps/zen-${name}.png
-      install -D $src/browser/chrome/icons/default/default48.png $out/share/icons/hicolor/48x48/apps/zen-${name}.png
-      install -D $src/browser/chrome/icons/default/default64.png $out/share/icons/hicolor/64x64/apps/zen-${name}.png
-      install -D $src/browser/chrome/icons/default/default128.png $out/share/icons/hicolor/128x128/apps/zen-${name}.png
-    '';
+    installPhase = if stdenv.hostPlatform.isDarwin
+      then installDarwin
+      else installLinux;
 
     passthru = {
       inherit applicationName binaryName libName;
@@ -123,7 +155,6 @@ in
       changelog = "https://github.com/zen-browser/desktop/releases";
       sourceProvenance = with lib.sourceTypes; [binaryNativeCode];
       platforms = builtins.attrNames mozillaPlatforms;
-      broken = stdenv.hostPlatform.isDarwin;
       hydraPlatforms = [];
       mainProgram = binaryName;
     };

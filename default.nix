1598159028e9b328af884abf357431fdca8c32da
isabel <isabel@isabelroses.com>
2025-08-17 13:38:20 +0100

    refactor(default.nix): remove redundant args

--- default.nix
+++ default.nix
@@ -2,16 +2,16 @@
   pkgs ? import <nixpkgs> {},
   system ? pkgs.stdenv.hostPlatform.system,
 }: let
-  mkZen = pkgs: name: system: entry: let
+  mkZen = name: entry: let
     variant = (builtins.fromJSON (builtins.readFile ./sources.json)).${entry}.${system};
   in
     pkgs.callPackage ./package.nix {
       inherit name variant;
     };
 in rec {
-  beta-unwrapped = mkZen pkgs "beta" system "beta";
-  twilight-unwrapped = mkZen pkgs "twilight" system "twilight";
-  twilight-official-unwrapped = mkZen pkgs "twilight" system "twilight-official";
+  beta-unwrapped = mkZen "beta" "beta";
+  twilight-unwrapped = mkZen "twilight" "twilight";
+  twilight-official-unwrapped = mkZen "twilight" "twilight-official";
 
   beta = pkgs.wrapFirefox beta-unwrapped {};
   twilight = pkgs.wrapFirefox twilight-unwrapped {};

7d454624512b95540ef3f43d89918f4ae0e56bb1
isabel <isabel@isabelroses.com>
2025-08-17 19:28:24 +0100

    cleanup (#88)
    * refactor(package): use package fetchers
    
    builtin fetchers are blocking, therefore also slow. for packaging we
    should use the package fetchers
    
    * refactor: build the desktop file as a part of the package
    
    * refactor(default.nix): remove redundant args
    
    * refactor(flake.nix): remove duplicated code & rec
    
    * fix(hm-module)!: typo in mkFireFoxmodule
    
    * fix(hm-module): work with nixpkgs config `allowAliases = false;`
    
    * fix: partial revert of 805c8f56e8ebac1527176fc9d551f73c4cd886f6
    
    the `lib.mkDefault` was being ignored and the policies would never be
    set. perhaps a better thing to do here would be to remove them
    completly. otherwise this is the next best thing
    
    * refactor(package): use `hash` over `sha256`
    
    Co-authored-by: Seth Flynn <getchoo@tuta.io>
    
    ---------
    
    Co-authored-by: Seth Flynn <getchoo@tuta.io>

--- hm-module.nix
+++ hm-module.nix
@@ -20,8 +20,8 @@ in {
     (mkFirefoxModule {
       inherit modulePath;
       name = applicationName;
-      wrappedPackageName = "zen-${name}-unwrapped";
-      unwrappedPackageName = "zen-${name}";
+      wrappedPackageName = "zen-${name}";
+      unwrappedPackageName = "zen-${name}-unwrapped";
       visible = true;
       platforms = {
         linux = {
@@ -38,7 +38,7 @@ in {
   config = lib.mkIf config.programs.zen-browser.enable {
     programs.zen-browser = {
       package =
-        (pkgs.wrapFirefox (self.packages.${pkgs.stdenv.system}."${name}-unwrapped".override {
+        (pkgs.wrapFirefox (self.packages.${pkgs.stdenv.hostPlatform.system}."${name}-unwrapped".override {
           # Seems like zen uses relative (to the original binary) path to the policies.json file
           # and ignores the overrides by pkgs.wrapFirefox
           policies = config.programs.zen-browser.policies;
@@ -47,10 +47,9 @@ in {
           nativeMessagingHosts = config.programs.zen-browser.nativeMessagingHosts;
         };
 
-      # This does not work, the package can't build using these policies
-      policies = lib.mkDefault {
-        DisableAppUpdate = true;
-        DisableTelemetry = true;
+      policies = {
+        DisableAppUpdate = lib.mkDefault true;
+        DisableTelemetry = lib.mkDefault true;
       };
     };
   };
